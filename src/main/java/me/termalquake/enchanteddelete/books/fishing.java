f7b83cbeb0ca77e8a6bbdf29f682c2bb6c583d5c
Sergey Velikonsky <94070808+TermalQuake@users.noreply.github.com>
2022-11-01 20:03:22 +0300

    Update fishing.java

--- src/main/java/me/termalquake/enchanteddelete/books/fishing.java
+++ src/main/java/me/termalquake/enchanteddelete/books/fishing.java
@@ -1,6 +1,5 @@
 package me.termalquake.enchanteddelete.books;
 
-
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
@@ -8,17 +7,20 @@ import org.bukkit.Material;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.entity.Item;
 
-
 public class fishing implements Listener {
     @EventHandler
     public void onPlayerFish(PlayerFishEvent e) {
         if (e.getCaught() instanceof Item) {
             ItemStack fished = ((Item) e.getCaught()).getItemStack();
-            if (fished.getType() == Material.ENCHANTED_BOOK || fished.getType() == Material.BOW || fished.getType() == Material.FISHING_ROD)   {
+           // if (fished.getType() == Material.ENCHANTED_BOOK)
+              if (fished.getEnchantments().size() > 0)
+                {
+                    e.getCaught().remove();
+                }
+            if (fished.getType() == Material.ENCHANTED_BOOK)
+            {
                 e.getCaught().remove();
             }
         }
     }
 }
-
-
