45606fc9aa32543db2f81d68d8fba0c0b14dc180
Sergey Velikonsky <94070808+TermalQuake@users.noreply.github.com>
2022-11-01 17:46:09 +0300

    detele fishing
    remove ENCHANTED_BOOK,BOW and FISHING_ROD

--- src/main/java/me/termalquake/enchanteddelete/books/fishing.java
+++ src/main/java/me/termalquake/enchanteddelete/books/fishing.java
@@ -1,8 +1,33 @@
 package me.termalquake.enchanteddelete.books;
 
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.entity.Item;
+
 
 public class fishing implements Listener {
-
+    @EventHandler
+    public void onPlayerFish(PlayerFishEvent e) {
+        if (e.getCaught() instanceof Item) {
+            ItemStack fished = ((Item) e.getCaught()).getItemStack();
+            if (fished.getType() == Material.ENCHANTED_BOOK) {
+                e.getCaught().remove();
+            }
+            if (fished.getType() == Material.BOW)  {
+                {
+                    e.getCaught().remove();
+                }
+                if (fished.getType() == Material.FISHING_ROD)  {
+                    {
+                        e.getCaught().remove();
+                    }
+                }
+            }
+        }
+    }
 }
+
