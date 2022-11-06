1b4a88dac2bf644314d05ae74b61e3893f91e15d
Sergey Velikonsky <94070808+TermalQuake@users.noreply.github.com>
2022-11-06 05:09:55 +0300

    rename items

--- src/main/java/me/termalquake/enchanteddelete/EnchantedDelete.java
+++ src/main/java/me/termalquake/enchanteddelete/EnchantedDelete.java
@@ -1,8 +1,10 @@
 package me.termalquake.enchanteddelete;
 
-import me.termalquake.enchanteddelete.books.fishing;
-import me.termalquake.enchanteddelete.books.table;
-import me.termalquake.enchanteddelete.books.villager;
+import me.termalquake.enchanteddelete.items.chest;
+import me.termalquake.enchanteddelete.items.fishing;
+import me.termalquake.enchanteddelete.items.table;
+import me.termalquake.enchanteddelete.items.villager;
+import me.termalquake.enchanteddelete.items.mobs;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnchantedDelete extends JavaPlugin {
@@ -12,6 +14,7 @@ public final class EnchantedDelete extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new villager(), this);
         this.getServer().getPluginManager().registerEvents(new fishing(), this);
         this.getServer().getPluginManager().registerEvents(new table(), this);
+        this.getServer().getPluginManager().registerEvents(new chest(), this);
         this.getServer().getPluginManager().registerEvents(new mobs(), this);
     }
 
