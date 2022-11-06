0cf20bfc296e6b70cb9b0576878ac485f33c4028
Sergey Velikonsky <94070808+TermalQuake@users.noreply.github.com>
2022-11-06 19:41:54 +0300

    Release 1.0

---
+++ src/main/java/me/termalquake/enchantdelete/EnchantDelete.java
@@ -0,0 +1,25 @@
+package me.termalquake.enchantdelete;
+
+import me.termalquake.enchantdelete.items.chest;
+import me.termalquake.enchantdelete.items.fishing;
+import me.termalquake.enchantdelete.items.table;
+import me.termalquake.enchantdelete.items.villager;
+import me.termalquake.enchantdelete.items.mobs;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchantDelete extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new villager(), this);
+        this.getServer().getPluginManager().registerEvents(new fishing(), this);
+        this.getServer().getPluginManager().registerEvents(new table(), this);
+        this.getServer().getPluginManager().registerEvents(new chest(), this);
+        this.getServer().getPluginManager().registerEvents(new mobs(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
